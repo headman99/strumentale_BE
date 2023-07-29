fb1cb6ec0e9de10de39591335f05677e07b42120
Davide Capacchione <davide.capacchione@gmail.com>
2023-07-29 19:49:48 +0200

    fruitcaake CORS

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
+        //in order to make CSRF token works you need to  eliminate both the value in the array and move the frontend app in the same subdomain of the backend app otherwhise laravel is not able to share cookies ascross different domains
         '/*',
         '*'
     ];
