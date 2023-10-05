b5f81c1d857896de7c5b2304c2d8b0acfd53589c
Davide Capacchione <davide.capacchione@gmail.com>
2023-10-05 18:07:14 +0200

    modifiche funzione get_result

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Item;
 use App\Models\Result;
 use App\Models\Survey;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
@@ -19,16 +20,16 @@ class UserController extends Controller
         $validate = $request->validate([
             'title' => ["nullable", "sometimes", "string", "max:100"],
             "text" => ['required', "string", "max:400"],
-            
+
         ]);
 
         try {
 
             //Count how many survey there are for a certain user
             $surveys =  Survey::where("user", $request->user()->id)->get();
-            if($surveys->count() >=20)
+            if ($surveys->count() >= 20)
                 throw ValidationException::withMessages(['error' => 'Non puoi salvare più di 20 ricerche.']);
-            
+
             Survey::create(array_merge(
                 $validate,
                 [
@@ -88,12 +89,12 @@ class UserController extends Controller
     public function update_survey(Request $request, string $id = null)
     {
         $validate = $request->validate([
-            "id" => ["integer", "sometimes", "required"], 
-            "title" => ["string","max:100"]
+            "id" => ["integer", "sometimes", "required"],
+            "title" => ["string", "max:100"]
         ]);
 
         try {
-            Survey::find($request->id)->update(['title'=> $request->title]);
+            Survey::find($request->id)->update(['title' => $request->title]);
             return response()->noContent();
         } catch (\Exception $exc) {
             Log::error($exc->getMessage());
@@ -101,20 +102,20 @@ class UserController extends Controller
         }
     }
 
-    
+
     public function save_item(Request $request): Response
     {
         $validate = $request->validate([
-            "name" => ["required","string"],
-            "url" => ["required","string"],
-            "img" => ["sometimes",'nullable']
+            "name" => ["required", "string"],
+            "url" => ["required", "string"],
+            "img" => ["sometimes", 'nullable']
         ]);
 
         try {
             $items =  Item::where("user", $request->user()->id)->get();
-            if($items->count() >=20)
+            if ($items->count() >= 20)
                 throw ValidationException::withMessages(['error' => 'Non puoi salvare piu di 20 oggetti.']);
-            
+
             Item::create(array_merge(
                 $validate,
                 ["user" => $request->user()->id]
@@ -134,7 +135,7 @@ class UserController extends Controller
         ]);
 
         try {
-            if (!$request->id) 
+            if (!$request->id)
                 Item::where("user", $request->user()->id)->delete();
 
             if ($request->id) {
@@ -162,12 +163,12 @@ class UserController extends Controller
                 $item = Item::find($request->id);
                 if (!$item)
                     throw ValidationException::withMessages(['error' => 'Parametri non validi']);
-                
+
                 if (!$item->user == $request->user()->id)
                     throw ValidationException::withMessages(['error' => 'Parametri non validi']);
                 $items = $item;
-            }else{
-                $items = Item::where("user",$request->user()->id)->get();
+            } else {
+                $items = Item::where("user", $request->user()->id)->get();
             }
 
             return response()->json($items);
@@ -176,7 +177,7 @@ class UserController extends Controller
             return response(['message' => "Qualcosa è andato storto, riprova", "exception" => $exc->getMessage()], \Illuminate\Http\Response::HTTP_BAD_REQUEST);
         }
     }
-    
+
 
     public function save_result(Request $request): Response
     {
@@ -234,14 +235,17 @@ class UserController extends Controller
         }
     }
 
-    public function get_result(Request $request, string $id = null): JsonResponse
+    public function get_result(Request $request, string $id = null) //: JsonResponse
     {
         $validate = $request->validate([
             //"id" => ["integer", "nullable", "sometimes"],
             //"item" => ["integer", "nullable", "sometimes"],
             "survey" => ["integer", "nullable", "sometimes"],
             "latest" => ["boolean", 'nullable', "sometimes"],
-            "cheapest" => ["boolean", "nullable", "sometimes"]
+            "cheapest" => ["boolean", "nullable", "sometimes"],
+            "samples" => ["sometimes", 'nullable', "integer"],
+            //nel formato Y-m-d
+            "n_months" => ["sometimes", "nullable", 'integer']
         ]);
 
         try {
@@ -260,10 +264,31 @@ class UserController extends Controller
                 throw ValidationException::withMessages(['error' => 'Parametri non validi']);
             if ($request->latest)
                 //complete
-            if ($request->cheapest)
-                //complete
-            $result = Result::where("survey", $request->survey)->get();
-            return response()->json($result);
+                if ($request->cheapest) {
+                }
+            //complete
+
+            $results = array();
+            if ($request->samples) {
+                //default 3 months
+                $n_months = $request->n_months!==null ? $request->n_months : 3;
+                $today = Carbon::today();
+                $endDate = Carbon::today()->subMonths($n_months);
+                $all_results = Result::where([
+                    ["survey", $request->survey],
+                    ["created_at", '<=', $today],
+                    ["created_at",">=", $endDate]
+                ])->orderBy("created_at", 'asc')->select("id","created_at","price")->get();
+                $all_results_count = $all_results->count();
+                $skip = round($all_results_count/$request->samples);
+                $results = $all_results->filter(function ($item, $index) use ($skip,$all_results_count) {
+                    return (($index % ($skip+1)) == 0) || ($index == $all_results_count - 1);
+                })->values();
+            } else {
+                $results = Result::where("survey", $request->survey)->get();
+            }
+
+            return response()->json($results);
         } catch (\Exception $exc) {
             Log::error($exc->getMessage());
             return response(['message' => "Qualcosa è andato storto, riprova", "exception" => $exc->getMessage()], \Illuminate\Http\Response::HTTP_BAD_REQUEST);
