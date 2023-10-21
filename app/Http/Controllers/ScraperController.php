98899a5f8f4bd9d9ada4acb392d2fcfe126ce4a7
Davide Capacchione <davide.capacchione@gmail.com>
2023-10-21 11:24:01 +0200

    save_scrape_result modifications

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Result;
 use App\Models\Survey;
+use Carbon\Carbon;
+use Carbon\CarbonInterval;
 use GuzzleHttp\Client;
 use GuzzleHttp\Promise\Promise;
 use GuzzleHttp\Promise\Utils;
@@ -19,8 +21,17 @@ class ScraperController extends Controller
     public function save_scrape_result(Request $request)
     {
         try{
-
-            DB::table("result")->insert($request->data);
+            $data = array_map(function ($result) {
+                return [
+                    'name' => $result->name,
+                    "survey" =>$result->survey,
+                    "price" => $result->price,
+                    "url" => $result->url,
+                    "created_at"=> Carbon::now(),
+                    "updated_at" => Carbon::now()
+                ];
+            },$request->data);
+            DB::table("result")->insert($data);
             return response(["status"=>true,"message" => 'everything is great']);
             //return response($request->data);
         }catch(\Exception $ex){
