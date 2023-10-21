4b9931cf50fe8070ad8d1c69314998e9224f8e05
Davide Capacchione <davide.capacchione@gmail.com>
2023-10-21 12:01:33 +0200

    cookies fix

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -37,8 +37,8 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): Response
     {
-        Cookie::queue(Cookie::forget('laravel_session'));
-        Cookie::queue(Cookie::forget('loggedin'));
+        Cookie::forget('laravel_session');
+        Cookie::forget('loggedin');
         $request->user()->tokens()->delete();
         $request->session()->invalidate();
         Auth::guard('web')->logout();
